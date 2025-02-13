1c076a5136d74547e29496b034c43a828784904f
JonathanBret <bret.jonathan@outlook.fr>
2025-02-13 22:56:55 +0100

    change code files

--- src/components/LogementInfo.jsx
+++ src/components/LogementInfo.jsx
@@ -1,23 +1,60 @@
-import { useParams } from "react-router-dom";
-import logements from "../data/logements.json";
-import "../style/Logement.scss";
-import Header from "../components/Header";
-import LogementInfo from "../components/LogementInfo";
+// src/components/LogementInfo.jsx
+import PropTypes from "prop-types";
+import DropdownListLogement from "./DropdownListLogement";
 
-const Logement = () => {
-  const { id } = useParams();
-  const logement = logements.find((item) => item.id === id);
-
-  if (!logement) {
-    return <div>Logement non trouvé</div>;
-  }
-
+const LogementInfo = ({ logement }) => {
   return (
-    <>
-      <Header />
-      <LogementInfo logement={logement} />
-    </>
+    <div className="logement-page">
+      <img src={logement.cover} alt={logement.title} className="logement-image" />
+
+      <div className="logement-info">
+        <div className="logement-details">
+          <h1>{logement.title}</h1>
+          <p className="location">{logement.location}</p>
+          <div className="tags">
+            {logement.tags.map((tag, index) => (
+              <span key={index} className="tag">{tag}</span>
+            ))}
+          </div>
+        </div>
+
+        <div className="logement-host">
+          <div className="host-info">
+            <p>{logement.host.name}</p>
+            <img src={logement.host.picture} alt={logement.host.name} className="host-picture" />
+          </div>
+          <div className="rating">
+            {[...Array(5)].map((_, i) => (
+              <span key={i} className={i < logement.rating ? "star filled" : "star"}>★</span>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {/* Ajouter le composant DropdownListLogement */}
+      <DropdownListLogement 
+        description={logement.description} 
+        equipements={logement.equipments} 
+      />
+    </div>
   );
 };
 
-export default Logement;
+// Validation des props
+LogementInfo.propTypes = {
+  logement: PropTypes.shape({
+    cover: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+    host: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      picture: PropTypes.string.isRequired,
+    }).isRequired,
+    rating: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+    equipments: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }).isRequired,
+};
+
+export default LogementInfo;
--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import logements from "../data/logements.json";
 import "../style/Logement.scss";
 import Header from "../components/Header";
-import DropdownListLogement from "../components/DropdownListLogement";
+import LogementInfo from "../components/LogementInfo";
 
 const Logement = () => {
   const { id } = useParams();
@@ -16,39 +16,7 @@ const Logement = () => {
   return (
     <>
       <Header />
-      <div className="logement-page">
-        <img src={logement.cover} alt={logement.title} className="logement-image" />
-
-        <div className="logement-info">
-          <div className="logement-details">
-            <h1>{logement.title}</h1>
-            <p className="location">{logement.location}</p>
-            <div className="tags">
-              {logement.tags.map((tag, index) => (
-                <span key={index} className="tag">{tag}</span>
-              ))}
-            </div>
-          </div>
-
-          <div className="logement-host">
-            <div className="host-info">
-              <p>{logement.host.name}</p>
-              <img src={logement.host.picture} alt={logement.host.name} className="host-picture" />
-            </div>
-            <div className="rating">
-              {[...Array(5)].map((_, i) => (
-                <span key={i} className={i < logement.rating ? "star filled" : "star"}>★</span>
-              ))}
-            </div>
-          </div>
-        </div>
-
-        {/* Ajouter le composant DropdownListLogement */}
-        <DropdownListLogement 
-          description={logement.description} 
-          equipements={logement.equipments} 
-        />
-      </div>
+      <LogementInfo logement={logement} />
     </>
   );
 };
