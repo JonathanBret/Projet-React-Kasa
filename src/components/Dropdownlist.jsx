49d0eb4dbb7f2cbb5f34fd0dee73813d3563662b
JonathanBret <bret.jonathan@outlook.fr>
2025-02-18 20:28:55 +0100

    text and style dropdown

--- src/components/Dropdownlist.jsx
+++ src/components/Dropdownlist.jsx
@@ -3,10 +3,10 @@ import "../style/DropdownList.scss";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa"; 
 
 const data = [
-  { title: "Fiabilité", content: "Nous garantissons des annonces vérifiées pour une confiance totale." },
-  { title: "Respect", content: "Le respect de chaque utilisateur est une priorité sur notre plateforme." },
-  { title: "Service", content: "Nous offrons un service client réactif et à l'écoute." },
-  { title: "Sécurité", content: "La sécurité des transactions et des données est notre engagement." }
+  { title: "Fiabilité", content: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." },
+  { title: "Respect", content: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." },
+  { title: "Service", content: "La qualité des services est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance." },
+  { title: "Sécurité", content: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." }
 ];
 
 const DropdownList = () => {
