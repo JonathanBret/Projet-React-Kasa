3e4b6a0c5073dd76a93df9a97fda9b7ca0590115
JonathanBret <bret.jonathan@outlook.fr>
2025-04-12 01:52:33 +0200

    fix build sass

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import sass from 'vite-plugin-sass';
 
 export default defineConfig({
-  plugins: [react(), sass()],
+  plugins: [react()],
 });
